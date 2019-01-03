5d084c37be8f71e1202ec3fd7700c740c92f9b28
luoyangC <luoyang15261829198@gmail.com>
2019-01-03 19:59:42 +0800

    项目初始化

---
+++ layouts/default.vue
@@ -0,0 +1,51 @@
+<template>
+  <v-app>
+    <v-navigation-drawer :clipped="clipped" v-model="drawer" fixed app>
+      <v-list>
+        <v-list-tile v-for="(item, i) in items" :to="item.to" :key="i" router exact>
+          <v-list-tile-action>
+            <v-icon v-html="item.icon"/>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title v-text="item.title"/>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar :clipped-left="clipped" fixed app>
+      <v-toolbar-side-icon @click="drawer = !drawer"/>
+      <v-toolbar-title v-text="title"/>
+    </v-toolbar>
+    <v-content>
+      <v-container>
+        <nuxt/>
+      </v-container>
+    </v-content>
+    <v-footer>
+      <v-spacer></v-spacer>
+      <span>&copy; 2017</span>
+      <v-spacer></v-spacer>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      clipped: true,
+      drawer: false,
+      items: [
+        { icon: "apps", title: "Welcome", to: "/" },
+        { icon: "bubble_chart", title: "Inspire", to: "/inspire" }
+      ],
+      right: true,
+      title: "Amor"
+    };
+  }
+};
+</script>
+
+<style lang="stylus">
+</style>
+
